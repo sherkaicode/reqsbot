958fef2faffdd71692c9385a528fa24cfa0f1665
sherkaicode <fljumawan@gmail.com>
2021-03-05 15:27:19 +0800

    Update roles /react.js

--- counters/react.js
+++ counters/react.js
@@ -1,13 +1,15 @@
 module.exports = async (client, Discord) => {
     const channel = '814420531599638529';
     const guild = client.guilds.cache.get('790447283921616916');
-    const pok = guild.roles.cache.find(role => role.name === 'Pokemon')
+
+    const game = guild.roles.cache.find(role => role.name === 'Gaming')
     const jap = guild.roles.cache.find(role => role.name === 'Japanese')
-    const sk = guild.roles.cache.find(role => role.name === 'Soul Knight')
+    const sch = guild.roles.cache.find(role => role.name === 'School')
+    
 
     const j = '🍥'
-    const p = '🔥'
-    const s = '🗡️'
+    const g = '🎮'
+    const s = '📝'
 
 
     let embed = new Discord.MessageEmbed()
@@ -15,15 +17,15 @@ module.exports = async (client, Discord) => {
         .setTitle('Choose a Role you want')
         .setDescription(`Choosing a role will let you access the channel\n\n`
             + `${j} for Japanese\n`
-            + `${p} for Pokemon\n`
-            + `${s} for Soul Knight`);
+            + `${g} for Gaming\n`
+            + `${s} for School`);
 
     guild.channels.cache.get('814420531599638529').messages.fetch({ limit: 1 }).then(messages => {
         guild.channels.cache.get('814420531599638529').bulkDelete(messages);
     })
     let messageEmbed = await guild.channels.cache.get('814420531599638529').send(embed)
     messageEmbed.react(j);
-    messageEmbed.react(p);
+    messageEmbed.react(g);
     messageEmbed.react(s);
 
     client.on('messageReactionAdd', async (reaction, user) => {
@@ -37,11 +39,11 @@ module.exports = async (client, Discord) => {
             if (reaction.emoji.name === j) {
                 await reaction.message.guild.members.cache.get(user.id).roles.add(jap)
             }
-            if (reaction.emoji.name === p) {
-                await reaction.message.guild.members.cache.get(user.id).roles.add(pok)
+            if (reaction.emoji.name === g) {
+                await reaction.message.guild.members.cache.get(user.id).roles.add(game)
             }
             if (reaction.emoji.name === s) {
-                await reaction.message.guild.members.cache.get(user.id).roles.add(sk)
+                await reaction.message.guild.members.cache.get(user.id).roles.add(sch)
             }
         } else {
             return;
@@ -59,11 +61,11 @@ module.exports = async (client, Discord) => {
             if (reaction.emoji.name === j) {
                 await reaction.message.guild.members.cache.get(user.id).roles.remove(jap)
             }
-            if (reaction.emoji.name === p) {
-                await reaction.message.guild.members.cache.get(user.id).roles.remove(pok)
+            if (reaction.emoji.name === g) {
+                await reaction.message.guild.members.cache.get(user.id).roles.remove(game)
             }
             if (reaction.emoji.name === s) {
-                await reaction.message.guild.members.cache.get(user.id).roles.remove(sk)
+                await reaction.message.guild.members.cache.get(user.id).roles.remove(sch)
             }
         } else {
             return;
