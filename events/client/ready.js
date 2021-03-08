6b6437f3e9a3b228de8dcd91535b0a16e9fa0af7
sherkaicode <fljumawan@gmail.com>
2021-03-08 13:42:50 +0800

    Hori play count

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,11 +1,13 @@
 const memberCounter = require('C:/Users/fljum/OneDrive/Desktop/Aegis/JSscript/Reqbot/counters/counterM.js')
-const serverTime = require('C:/Users/fljum/OneDrive/Desktop/Aegis/JSscript/Reqbot/counters/time.js')
+//const serverTime = require('C:/Users/fljum/OneDrive/Desktop/Aegis/JSscript/Reqbot/counters/time.js')
 const reactRole = require('C:/Users/fljum/OneDrive/Desktop/Aegis/JSscript/Reqbot/counters/react.js')
+const count = require('C:/Users/fljum/OneDrive/Desktop/Aegis/JSscript/Reqbot/counters/countto.js')
 module.exports = (Discord, client) => {
 
     console.log("Ready")
     memberCounter(client);
-    serverTime(client);
+    //serverTime(client);
     reactRole(client, Discord)
+    //count(client)
 
 }
--- events/guild/message.js
+++ events/guild/message.js
@@ -1,7 +1,15 @@
 const { includes } = require("ffmpeg-static");
+const count = require('C:/Users/fljum/OneDrive/Desktop/Aegis/JSscript/Reqbot/counters/countto.js')
 
 module.exports = (Discord, client, message) => {
+    
+    
     const prefix =  process.env.PREFIX+" ";
+    if (message) {
+        if (message.channel == '817272342307930142'&& !message.author.bot) {
+            count(client);
+        }
+    }
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
