fc74f8af8733332026837948ee1d09e881fb6fe1
sherkaicode <fljumawan@gmail.com>
2021-03-11 11:29:55 +0800

    Upgraded

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,23 +1,28 @@
 const Canvas = require('canvas')
 module.exports = async (Discord, client, member) => {
-    const applyText = (canvas, text) => {
-        const ctx = canvas.getContext('2d');
-    
-        let fontSize = 70;
-    
-        do {
-            ctx.font = `${fontSize -= 10}px sans-serif`;
-           
-        } while (ctx.measureText(text).width > canvas.width - 300);
-    
-        return ctx.font;
-    };
-    
-    const channel = member.guild.channels.cache.find(ch => ch.name === '📔│log');
+	const applyText = (canvas, text) => {
+		const ctx = canvas.getContext('2d');
+
+		let fontSize = 70;
+
+		do {
+			ctx.font = `${fontSize -= 10}px sans-serif`;
+
+		} while (ctx.measureText(text).width > canvas.width/2);
+
+		return ctx.font;
+	};
+	const getPos = (canvas, text) => {
+		const ctx = canvas.getContext('2d');
+
+		return (canvas.width/2 - ctx.measureText(text).width)/2
+	}
+
+	const channel = member.guild.channels.cache.find(ch => ch.name === '📔│log');
 	if (!channel) return;
 
-    let wcRole = member.guild.roles.cache.find(role => role.name === 'Member');
-    member.roles.add(wcRole);
+	let wcRole = member.guild.roles.cache.find(role => role.name === 'Member');
+	member.roles.add(wcRole);
 	const canvas = Canvas.createCanvas(700, 250);
 	const ctx = canvas.getContext('2d');
 
@@ -29,22 +34,22 @@ module.exports = async (Discord, client, member) => {
 
 	ctx.font = '28px sans-serif';
 	ctx.fillStyle = '#000000.';
-	ctx.fillText('WELCOME', canvas.width / 2.7, canvas.height / 3.5);
+	ctx.fillText('WELCOME', getPos(canvas, 'WELCOME'), canvas.height - 100);
 
 	ctx.font = applyText(canvas, `${member.displayName}!`);
 	ctx.fillStyle = '#000000.';
-	ctx.fillText(`${member.displayName}!`, canvas.width / 2.8, canvas.height / 1.8);
-
+	ctx.fillText(`${member.displayName}!`, getPos(canvas, `${member.displayName}!`), canvas.height - 50);
+	
 	ctx.beginPath();
-	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+	ctx.arc(canvas.width / 4, 75, 50, 0, Math.PI * 2, true);
 	ctx.closePath();
 	ctx.clip();
 
 	const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
-	ctx.drawImage(avatar, 25, 25, 200, 200);
+	ctx.drawImage(avatar, canvas.width / 4 - 50, 25, 100, 100);
 
 	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
 
-    member.guild.channels.cache.get('813679705638305813').send(`Welcome to the server, ${member}!`, attachment)
+	member.guild.channels.cache.get('813679705638305813').send(`Welcome to the server, ${member}!`, attachment)
 
 }
