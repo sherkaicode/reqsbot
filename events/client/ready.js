9b5790d97af9eb76776b2623d494ec26cdb55f30
sherkaicode <fljumawan@gmail.com>
2021-03-12 12:47:52 +0800

    Improving the Guild Add

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,12 +1,14 @@
 const memberCounter = require('C:/Users/fljum/OneDrive/Desktop/Aegis/JSscript/Reqbot/counters/counterM.js')
 const reactRole = require('C:/Users/fljum/OneDrive/Desktop/Aegis/JSscript/Reqbot/counters/react.js')
-const count = require('C:/Users/fljum/OneDrive/Desktop/Aegis/JSscript/Reqbot/counters/countto.js')
+const time = require('C:/Users/fljum/OneDrive/Desktop/Aegis/JSscript/Reqbot/counters/time.js')
+
 
 module.exports = (Discord, client) => {
 
     console.log("Ready")
     memberCounter(client);
     reactRole(client, Discord)
+    time(client)
     client.user.setActivity("Mention me for help"); 
 
 }
--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,12 +1,13 @@
 const Canvas = require('canvas')
 module.exports = async (Discord, client, member) => {
+	console.log(member)
 	const applyText = (canvas, text) => {
 		const ctx = canvas.getContext('2d');
 
-		let fontSize = 70;
+		let fontSize = 60;
 
 		do {
-			ctx.font = `${fontSize -= 10}px sans-serif`;
+			ctx.font = `${fontSize -= 1}px "Bebas Neue"`;
 
 		} while (ctx.measureText(text).width > canvas.width/2);
 
@@ -17,12 +18,11 @@ module.exports = async (Discord, client, member) => {
 
 		return (canvas.width/2 - ctx.measureText(text).width)/2
 	}
-
-	const channel = member.guild.channels.cache.find(ch => ch.name === '📔│log');
-	if (!channel) return;
-
+	
 	let wcRole = member.guild.roles.cache.find(role => role.name === 'Member');
 	member.roles.add(wcRole);
+	console.log(member.role)
+	Canvas.registerFont('C:/Users/fljum/OneDrive/Desktop/Aegis/JSscript/Reqbot/Database/BN.ttf', { family: 'Bebas Neue' })
 	const canvas = Canvas.createCanvas(700, 250);
 	const ctx = canvas.getContext('2d');
 
@@ -32,24 +32,22 @@ module.exports = async (Discord, client, member) => {
 	ctx.strokeStyle = '#74037b';
 	ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
-	ctx.font = '28px sans-serif';
-	ctx.fillStyle = '#000000.';
+	ctx.font = '28px "Bebas Neue"';
+	ctx.fillStyle = '#FAF5E1';
 	ctx.fillText('WELCOME', getPos(canvas, 'WELCOME'), canvas.height - 100);
 
 	ctx.font = applyText(canvas, `${member.displayName}!`);
-	ctx.fillStyle = '#000000.';
+	ctx.fillStyle = '#FAF5E1';
 	ctx.fillText(`${member.displayName}!`, getPos(canvas, `${member.displayName}!`), canvas.height - 50);
-	
-	ctx.beginPath();
-	ctx.arc(canvas.width / 4, 75, 50, 0, Math.PI * 2, true);
-	ctx.closePath();
-	ctx.clip();
+	// ctx.beginPath();
+	// ctx.arc(canvas.width / 4, 75, 50, 0, Math.PI * 2, true);
+	// ctx.closePath();
+	// ctx.clip();
 
 	const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
-	ctx.drawImage(avatar, canvas.width / 4 - 50, 25, 100, 100);
+	ctx.drawImage(avatar, 15 , canvas.height/2 - 50, 100, 100);
 
 	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
 
 	member.guild.channels.cache.get('813679705638305813').send(`Welcome to the server, ${member}!`, attachment)
-
 }
