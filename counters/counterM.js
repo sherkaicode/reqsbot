134264709e5664d53b91f942e2c51ebf00ad44bf
sherkaicode <fljumawan@gmail.com>
2021-03-14 16:29:48 +0800

    Updated Time

--- counters/counterM.js
+++ counters/counterM.js
@@ -2,9 +2,9 @@ module.exports = async (client) => {
     const guild = client.guilds.cache.get('790447283921616916');
     setInterval(() => {
         const memberCount = guild.memberCount;
-        const channel = guild.channels.cache.get('815080759714119700')
+        const channel = guild.channels.cache.get('818403289091670027')
 
-        channel.setName(`Total Members: ${memberCount.toLocaleString()}`)
+        channel.setName(`👥│ ${memberCount.toLocaleString()} Members`)
         //console.log(memberCount);
     }, 5000);
 }
