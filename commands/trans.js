797113e400cc061f2157ce21977671d087204692
sherkaicode <fljumawan@gmail.com>
2021-04-02 15:01:17 +0800

    Added Translator from jp to english && update log image

---
+++ commands/trans.js
@@ -0,0 +1,14 @@
+const translate = require('translate')
+translate.engine = "libre";
+
+module.exports = {
+    name: 'trans',
+    description: "Translate Japanese",
+    execute(client, message, args, Discord) {
+        const trans = args[0];
+        translate(trans, { from: "ja", to: "en" }).then(text => {
+            console.log(text);
+            message.reply(`The translation is: `+ "\n" + '```' + text + '```') 
+        });
+    }
+}
