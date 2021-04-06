f3b34d7b7cdef5f224a86fb0841200056db72ef3
sherkaicode <fljumawan@gmail.com>
2021-04-06 14:38:21 +0800

    added cooldowns

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,10 +1,10 @@
 const { includes } = require("ffmpeg-static");
 const countv = require('../../counters/counts')
+const cooldowns = new Map();
 
 module.exports = (Discord, client, message) => {
-
-
-    const prefix = ',';
+    const prefix = '=';
+    
     if (message) {
 
 
@@ -30,6 +30,23 @@ module.exports = (Discord, client, message) => {
     const cmd = args.shift().toLocaleLowerCase();
 
     const command = client.commands.get(cmd) || client.commands.find(a => a.aliases && a.aliases.includes(cmd));
+
+    if (!cooldowns.has(command.name)){
+        cooldowns.set(command.name, new Discord.Collection());
+    }
+
+    const current_time = Date.now()
+    const time_stamps = cooldowns.get(command.name);
+    const cooldown_amount = (command.cooldown) * 1000;
+    if (time_stamps.has(message.author.id)) {
+        const expiration_time = time_stamps.get(message.author.id) + cooldown_amount;
+        if (current_time < expiration_time) {
+            const time_left = (expiration_time - current_time)/1000;
+            return message.reply(`Please wait for ${time_left.toFixed(1)} seconds before using ${command.name}`)
+        }
+    }
+    time_stamps.set(message.author.id, current_time);
+    setTimeout(()=> time_stamps.delete(message.author.id), cooldown_amount);
 
     try {
         command.execute(client, message, args, Discord);
