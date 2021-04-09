f1de7902f3ee5dddf5786672c372aac749143a30
sherkaicode <fljumawan@gmail.com>
2021-04-09 13:12:01 +0800

    error checking

--- events/guild/message.js
+++ events/guild/message.js
@@ -35,23 +35,24 @@ module.exports = (Discord, client, message) => {
         if (!cooldowns.has(command.name)) {
             cooldowns.set(command.name, new Discord.Collection());
         }
+        const current_time = Date.now()
+        const time_stamps = cooldowns.get(command.name);
+        const cooldown_amount = (command.cooldown) * 1000;
+        if (time_stamps.has(message.author.id)) {
+            const expiration_time = time_stamps.get(message.author.id) + cooldown_amount;
+            if (current_time < expiration_time) {
+                const time_left = (expiration_time - current_time) / 1000;
+                return message.reply(`Please wait for ${time_left.toFixed(1)} seconds before using ${command.name}`)
+            }
+        }
+        time_stamps.set(message.author.id, current_time);
+        setTimeout(() => time_stamps.delete(message.author.id), cooldown_amount);
     }
     catch (err) {
-        throw err;
+        message.channel.send('No command found')
     }
 
-    const current_time = Date.now()
-    const time_stamps = cooldowns.get(command.name);
-    const cooldown_amount = (command.cooldown) * 1000;
-    if (time_stamps.has(message.author.id)) {
-        const expiration_time = time_stamps.get(message.author.id) + cooldown_amount;
-        if (current_time < expiration_time) {
-            const time_left = (expiration_time - current_time) / 1000;
-            return message.reply(`Please wait for ${time_left.toFixed(1)} seconds before using ${command.name}`)
-        }
-    }
-    time_stamps.set(message.author.id, current_time);
-    setTimeout(() => time_stamps.delete(message.author.id), cooldown_amount);
+
 
     try {
         command.execute(client, message, args, Discord, cmd);
