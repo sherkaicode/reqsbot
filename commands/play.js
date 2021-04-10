c7406fd2197d1132a985f7fc31bfabebc72b812c
sherkaicode <fljumawan@gmail.com>
2021-04-10 09:41:20 +0800

    Added emoji to music player

--- commands/play.js
+++ commands/play.js
@@ -1,45 +1,43 @@
 const ytdl = require('ytdl-core');
 const ytSearch = require('yt-search');
 
-
 const queue = new Map();
 
 module.exports = {
     name: 'play',
-    aliases: ['skip', 'stop', 'q'], 
+    aliases: ['skip', 'stop', 'q'],
     cooldown: 0,
     description: 'Advanced music bot',
     async execute(client, message, args, Discord, cmd) {
+        const Roka = client.emojis.cache.get("829305452603899904")
 
-
-        
         const voice_channel = message.member.voice.channel;
         if (!voice_channel) return message.channel.send('You need to be in a channel to execute this command!');
         const permissions = voice_channel.permissionsFor(message.client.user);
         if (!permissions.has('CONNECT')) return message.channel.send('You dont have the correct permissins');
         if (!permissions.has('SPEAK')) return message.channel.send('You dont have the correct permissins');
 
-        
+
         const server_queue = queue.get(message.guild.id);
 
         if (cmd == 'q') {
             console.log(server_queue)
             console.log(server_queue.songs[0].title)
             var m = ''
-            for (var c = 0; c < server_queue.songs.length; c = c+1) {
-                m = `[${c+1}] ${server_queue.songs[c].title}` + '\n' + m;
+            for (var c = 0; c < server_queue.songs.length; c = c + 1) {
+                m = `[${c + 1}] ${server_queue.songs[c].title}` + '\n' + m;
             }
-            message.channel.send('```'+ m + '```');
+            message.channel.send('```' + m + '```');
         }
         if (cmd === 'play') {
             if (!args.length) return message.channel.send('You need to send the second argument!');
             let song = {};
-            
+
             if (ytdl.validateURL(args[0])) {
                 const song_info = await ytdl.getInfo(args[0]);
                 song = { title: song_info.videoDetails.title, url: song_info.videoDetails.video_url }
             } else {
-                
+
                 const video_finder = async (query) => {
                     const video_result = await ytSearch(query);
                     return (video_result.videos.length > 1) ? video_result.videos[0] : null;
@@ -53,7 +51,7 @@ module.exports = {
                 }
             }
 
-            
+
             if (!server_queue) {
 
                 const queue_constructor = {
@@ -63,11 +61,11 @@ module.exports = {
                     songs: []
                 }
 
-                
+
                 queue.set(message.guild.id, queue_constructor);
                 queue_constructor.songs.push(song);
 
-                
+
                 try {
                     const connection = await voice_channel.join();
                     queue_constructor.connection = connection;
@@ -79,7 +77,7 @@ module.exports = {
                 }
             } else {
                 server_queue.songs.push(song);
-                return message.channel.send(`👍 **${song.title}** added to queue!`);
+                return message.channel.send(`${Roka} **${song.title}** added to queue!`);
             }
         }
 
@@ -90,9 +88,10 @@ module.exports = {
 }
 
 const video_player = async (guild, song) => {
+    const chopper = client.emojis.cache.get('830253377740734464')
     const song_queue = queue.get(guild.id);
 
-    
+
     if (!song) {
         song_queue.voice_channel.leave();
         queue.delete(guild.id);
@@ -104,13 +103,14 @@ const video_player = async (guild, song) => {
             song_queue.songs.shift();
             video_player(guild, song_queue.songs[0]);
         });
-    await song_queue.text_channel.send(`🎶 Now playing **${song.title}**`)
+    await song_queue.text_channel.send(`${chopper} Now playing **${song.title}**`)
 }
 
 const skip_song = (message, server_queue) => {
+    const brook = client.emojis.cache.get('829310277194743829')
     if (!message.member.voice.channel) return message.channel.send('You need to be in a channel to execute this command!');
     if (!server_queue) {
-        return message.channel.send(`There are no songs in queue 😔`);
+        return message.channel.send(`There are no songs in queue ${brook}`);
     }
     server_queue.connection.dispatcher.end();
 }
@@ -119,4 +119,5 @@ const stop_song = (message, server_queue) => {
     if (!message.member.voice.channel) return message.channel.send('You need to be in a channel to execute this command!');
     server_queue.songs = [];
     server_queue.connection.dispatcher.end();
+    message.react('829307972832329749')
 }
