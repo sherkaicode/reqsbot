7c7db85e74f90045ea64942a4281409b7287e56a
sherkaicode <fljumawan@gmail.com>
2021-07-15 00:57:31 +0800

    Added new server

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,5 +1,10 @@
 const Canvas = require('canvas')
 module.exports = async (Discord, client, member) => {
+	console.log(member.guild.id)
+	const guild = client.guilds.cache.get('818503228076457984');
+
+	const memberCount = guild.memberCount;
+	const Name = `${member.displayName}#${member.user.discriminator}`;
 
 	const applyText = (canvas, text) => {
 		const ctx = canvas.getContext('2d');
@@ -14,48 +19,93 @@ module.exports = async (Discord, client, member) => {
 		const ctx = canvas.getContext('2d');
 		return (canvas.width - ctx.measureText(text).width) / 2
 	}
-	
-	const emoji = client.emojis.cache.get("829305452603899904")
-	let wcRole = member.guild.roles.cache.find(role => role.name === 'Member');
-	let botRole = member.guild.roles.cache.find(role => role.name === 'Bots')
-	if (!member.user.bot) {
-		member.roles.add(wcRole);
+	if (member.guild.id == '818503228076457984') {
+		Canvas.registerFont('Database/BN.ttf', { family: 'Compose Black' })
+		Canvas.registerFont('Database/JP.ttf', { family: 'Jap' })
+
+		const canvas = Canvas.createCanvas(1024, 500);
+		const ctx = canvas.getContext('2d');
+		const background = await Canvas.loadImage('Database/CB.jpg');
+		ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+		ctx.strokeStyle = '#ff6961';
+		ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+		ctx.font = applyText(canvas, `${Name}!`);
+		ctx.fillStyle = '#ffffff';
+		ctx.fillText(`${Name}`, getPos(canvas, `${Name}`), canvas.height - 40);
+		ctx.strokeStyle = '#000000';
+		ctx.lineWidth = 5;
+		ctx.strokeText(`${Name}`, getPos(canvas, `${Name}`), canvas.height - 40);
+
+
+		ctx.font = `70px "Jap"`;
+		ctx.fillStyle = '#ffffff';
+		ctx.fillText('ようこそ', getPos(canvas, 'ようこそ'), canvas.height - 130);
+		ctx.strokeStyle = '#000000';
+		ctx.lineWidth = 5;
+		ctx.strokeText('ようこそ', getPos(canvas, 'ようこそ'), canvas.height - 130);
+
+		ctx.beginPath();
+		ctx.arc(513, 175, 122, 0, Math.PI * 2, true);
+		ctx.closePath();
+		ctx.clip();
+
+		const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+		ctx.drawImage(avatar, 388, 50, 250, 250);
+
+		const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
+
+		console.log('swag')
+		member.guild.channels.cache.get('862673750997729301').send(`ようこそ, You're the ${memberCount + 1}th member! ${member}!`, attachment)
 	}
-	else {
-		member.roles.add(botRole);
-	}
-	Canvas.registerFont('Database/BN.ttf', { family: 'Compose Black' })
-	const canvas = Canvas.createCanvas(1024, 500);
-	const ctx = canvas.getContext('2d');
-	const background = await Canvas.loadImage('Database/Wall2.png');
-	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-	ctx.strokeStyle = '#ff6961';
-	ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
-	ctx.font = applyText(canvas, `${member.displayName}!`);
-	ctx.fillStyle = '#ffffff';
-	ctx.fillText(`${member.displayName}`, getPos(canvas, `${member.displayName}`), canvas.height - 40);
-	ctx.strokeStyle = '#000000';
-	ctx.lineWidth = 5;
-	ctx.strokeText(`${member.displayName}`, getPos(canvas, `${member.displayName}`), canvas.height - 40);
 
-	ctx.beginPath();
-	ctx.arc(513, 175, 122, 0, Math.PI * 2, true);
-	ctx.closePath();
-	ctx.clip();
+	if (member.guild.id == '790447283921616916') {
+		const emoji = client.emojis.cache.get("829305452603899904")
+		let wcRole = member.guild.roles.cache.find(role => role.name === 'Member');
+		let botRole = member.guild.roles.cache.find(role => role.name === 'Bots')
+		if (!member.user.bot) {
+			member.roles.add(wcRole);
+		}
+		else {
+			member.roles.add(botRole);
+		}
+		Canvas.registerFont('Database/BN.ttf', { family: 'Compose Black' })
+		Canvas.registerFont('Database/JP.ttf', { family: 'Jap' })
 
-	const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
-	ctx.drawImage(avatar, 388, 50, 250, 250);
+		const canvas = Canvas.createCanvas(1024, 500);
+		const ctx = canvas.getContext('2d');
+		const background = await Canvas.loadImage('Database/Wall2.png');
+		ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+		ctx.strokeStyle = '#ff6961';
+		ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
-	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
+		ctx.font = applyText(canvas, `${member.displayName}!`);
+		ctx.fillStyle = '#ffffff';
+		ctx.fillText(`${member.displayName}`, getPos(canvas, `${member.displayName}`), canvas.height - 40);
+		ctx.strokeStyle = '#000000';
+		ctx.lineWidth = 5;
+		ctx.strokeText(`${member.displayName}`, getPos(canvas, `${member.displayName}`), canvas.height - 40);
 
-	if (!member.user.bot) {
-		member.guild.channels.cache.get('813679705638305813').send(`Welcome to **Hanamaru** ${member}! ${emoji}` + "\n" +
-			`Please visit ${member.guild.channels.cache.get('814420531599638529').toString()} to get a role.`, attachment)
 
-	}
-	else {
-		member.guild.channels.cache.get('813679705638305813').send(`Welcome to **Hanamaru** ${member}! ${emoji}` + "\n" +
-			`Do your Job I hired you for`, attachment)
+		ctx.beginPath();
+		ctx.arc(513, 175, 122, 0, Math.PI * 2, true);
+		ctx.closePath();
+		ctx.clip();
+
+		const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+		ctx.drawImage(avatar, 388, 50, 250, 250);
+
+		const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
+
+		if (!member.user.bot) {
+			member.guild.channels.cache.get('824294522757447700').send(`Welcome to **Hanamaru** ${member}! ${emoji}` + "\n" +
+				`Please visit ${member.guild.channels.cache.get('814420531599638529').toString()} to get a role.`, attachment)
+
+		}
+		else {
+			member.guild.channels.cache.get('813679705638305813').send(`Welcome to **Hanamaru** ${member}! ${emoji}` + "\n" +
+				`Do your Job I hired you for`, attachment)
+		}
 	}
 }
--- events/guild/message.js
+++ events/guild/message.js
@@ -20,41 +20,48 @@ module.exports = (Discord, client, message) => {
             message.channel.send(m)
         };
     }
+
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const cmd = args.shift().toLocaleLowerCase();
+    if (!(message.guild.id == '818503228076457984')) {
 
-    const command = client.commands.get(cmd) || client.commands.find(a => a.aliases && a.aliases.includes(cmd));
 
-    try {
-        if (!cooldowns.has(command.name)) {
-            cooldowns.set(command.name, new Discord.Collection());
-        }
-        const current_time = Date.now()
-        const time_stamps = cooldowns.get(command.name);
-        const cooldown_amount = (command.cooldown) * 1000;
-        if (time_stamps.has(message.author.id)) {
-            const expiration_time = time_stamps.get(message.author.id) + cooldown_amount;
-            if (current_time < expiration_time) {
-                const time_left = (expiration_time - current_time) / 1000;
-                return message.reply(`Please wait for ${time_left.toFixed(1)} seconds before using ${command.name}`)
+        const command = client.commands.get(cmd) || client.commands.find(a => a.aliases && a.aliases.includes(cmd));
+
+        try {
+            if (!cooldowns.has(command.name)) {
+                cooldowns.set(command.name, new Discord.Collection());
             }
+            const current_time = Date.now()
+            const time_stamps = cooldowns.get(command.name);
+            const cooldown_amount = (command.cooldown) * 1000;
+            if (time_stamps.has(message.author.id)) {
+                const expiration_time = time_stamps.get(message.author.id) + cooldown_amount;
+                if (current_time < expiration_time) {
+                    const time_left = (expiration_time - current_time) / 1000;
+                    return message.reply(`Please wait for ${time_left.toFixed(1)} seconds before using ${command.name}`)
+                }
+            }
+            time_stamps.set(message.author.id, current_time);
+            setTimeout(() => time_stamps.delete(message.author.id), cooldown_amount);
         }
-        time_stamps.set(message.author.id, current_time);
-        setTimeout(() => time_stamps.delete(message.author.id), cooldown_amount);
+        catch (err) {
+            message.channel.send('No command found')
+        }
+
+
+
+        try {
+            command.execute(client, message, args, Discord, cmd);
+        } catch (err) {
+            message.reply("Error doing the command");
+            console.log(err);
+        }
     }
-    catch (err) {
-        message.channel.send('No command found')
-    }
-
-
-
-    try {
-        command.execute(client, message, args, Discord, cmd);
-    } catch (err) {
-        message.reply("Error doing the command");
-        console.log(err);
+    else {
+        console.log('Sorry not yet available')
     }
 
 }
